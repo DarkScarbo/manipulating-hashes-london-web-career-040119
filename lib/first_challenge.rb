9c656287a0f2cea34902917273fb583040abb69a
Xabier C <xabier_abia@hotmail.com>
2019-03-20 23:21:55 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,17 @@ def first_challenge
   }
 
   #your code here
-  contacts["Freddy Mercury"][:favorite_icecream_flavors]["strawberry"].shift
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      if attribute == :favourite_icecream_flavors
+        value.each do |flavor|
+          if flavor == "strawerry"
+            attribute.shift(value)
+          end
+        end
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
